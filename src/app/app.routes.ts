08c6c48f39bda8c3d29d09828ebad708818e6674
khajahussainucw <khajahussain120@gmail.com>
2025-04-01 20:42:38 -0700

    fixing routing problem.   standalone: true   imports: [RouterModule]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+//export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'about.html', component: AboutComponent }, // ✅ Support for /about.
+  { path: '**', redirectTo: '' }
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+/*  imports: [],*/
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [RouterModule],
 })
 export class HomeComponent {
 
