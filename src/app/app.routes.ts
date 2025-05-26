59cd6c92cfe566289cd296c2386f4c176bf7f383
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-05-25 18:21:27 -0700

    json to table plus page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { JsonToTableComponent } from './pages/json-to-table/json-to-table.component';
+import { JsonToTablePlusComponent } from './pages/json-to-table-plus/json-to-table-plus.component';
 
 //export const routes: Routes = [];
 
 
 export const routes: Routes = [
   { path: '', component: JsonToTableComponent },
+  { path: 'json-to-table-plus', component: JsonToTablePlusComponent },
   { path: 'about', component: AboutComponent },
   { path: 'about.html', component: AboutComponent }, // ✅ Support for /about.
   { path: '**', redirectTo: '' }
