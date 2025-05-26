aab56c895f7fb235aefc68eef33f1418501201b8
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-05-25 22:46:02 -0700

    json fixer

--- src/app/pages/json-fixer/json-fixer.component.ts
+++ src/app/pages/json-fixer/json-fixer.component.ts
@@ -10,5 +10,15 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./json-fixer.component.css']
 })
 export class JsonFixerComponent {
+  loadSampleData(): void {
+    // TODO: Load sample JSON into left editor
+  }
 
+  fixJson(): void {
+    // TODO: Fix or transform JSON from left editor to right editor
+  }
+
+  clearEditors(): void {
+    // TODO: Clear both left and right editors
+  }
 }
