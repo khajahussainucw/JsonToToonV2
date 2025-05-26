b8bd1c5a557a9954f5ff4a8c4640b893af82ac9b
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-05-25 22:20:05 -0700

    revmoving sorting option

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { JsonToTableComponent } from './pages/json-to-table/json-to-table.component';
-import { JsonToTablePlusComponent } from './pages/json-to-table-plus/json-to-table-plus.component';
 
 //export const routes: Routes = [];
 
 
 export const routes: Routes = [
   { path: '', component: JsonToTableComponent },
-  { path: 'json-to-table-plus', component: JsonToTablePlusComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'about.html', component: AboutComponent }, // ✅ Support for /about.
+  { path: 'about.html', component: AboutComponent }, // ✅ Support for /about.html
   { path: '**', redirectTo: '' }
 ];
