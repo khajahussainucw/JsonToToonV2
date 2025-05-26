27f4fa286d4a34b663dea5b740c81e0bc0dbd367
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-05-26 00:20:55 -0700

    removing aboutus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { JsonToTableComponent } from './pages/json-to-table/json-to-table.component';
 import { JsonFixerComponent } from './pages/json-fixer/json-fixer.component';
 
@@ -9,7 +8,5 @@ import { JsonFixerComponent } from './pages/json-fixer/json-fixer.component';
 export const routes: Routes = [
   { path: '', component: JsonToTableComponent },
   { path: 'json-fixer', component: JsonFixerComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'about.html', component: AboutComponent }, // ✅ Support for /about.html
   { path: '**', redirectTo: '' }
 ];
