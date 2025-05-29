74822a04788f428a8aed030b06114f52553bc278
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-05-29 00:01:55 -0700

    routing and height setting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { JsonToTableComponent } from './pages/json-to-table/json-to-table.component';
 import { JsonFixerComponent } from './pages/json-fixer/json-fixer.component';
 import { JsonFormatterComponent } from './pages/json-formatter/json-formatter.component';
+import { StringToJsonComponent } from './pages/string-to-json/string-to-json.component';
 
 //export const routes: Routes = [];
 
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: '', component: JsonToTableComponent },
   { path: 'json-fixer', component: JsonFixerComponent },
   { path: 'json-formatter', component: JsonFormatterComponent },
+  { path: 'string-to-json', component: StringToJsonComponent },
   { path: '**', redirectTo: '' }
 ];
