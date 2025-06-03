c68ff6755ee22b132d41e6850c30d40a70e150e6
KH <150889757+khajahussainucw@users.noreply.github.com>
2025-06-02 17:39:47 -0700

    enabling lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,37 @@
 import { Routes } from '@angular/router';
-import { JsonToTableComponent } from './pages/json-to-table/json-to-table.component';
-import { JsonFixerComponent } from './pages/json-fixer/json-fixer.component';
-import { JsonFormatterComponent } from './pages/json-formatter/json-formatter.component';
-import { StringToJsonComponent } from './pages/string-to-json/string-to-json.component';
-import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-
-//export const routes: Routes = [];
-
 
 export const routes: Routes = [
-  { path: '', component: JsonToTableComponent },
-  { path: 'index', component: JsonToTableComponent },
-  { path: 'json-to-table', component: JsonToTableComponent },
-  { path: 'json-fixer', component: JsonFixerComponent },
-  { path: 'json-formatter', component: JsonFormatterComponent },
-  { path: 'string-to-json', component: StringToJsonComponent },
-  { path: 'privacy-policy', component: PrivacyPolicyComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+  {
+    path: '',
+    loadComponent: () => import('./pages/json-to-table/json-to-table.component').then(m => m.JsonToTableComponent)
+  },
+  {
+    path: 'json-to-table',
+    loadComponent: () => import('./pages/json-to-table/json-to-table.component').then(m => m.JsonToTableComponent)
+  },
+  {
+    path: 'json-fixer',
+    loadComponent: () => import('./pages/json-fixer/json-fixer.component').then(m => m.JsonFixerComponent)
+  },
+  {
+    path: 'json-formatter',
+    loadComponent: () => import('./pages/json-formatter/json-formatter.component').then(m => m.JsonFormatterComponent)
+  },
+  {
+    path: 'string-to-json',
+    loadComponent: () => import('./pages/string-to-json/string-to-json.component').then(m => m.StringToJsonComponent)
+  },
+  {
+    path: 'privacy-policy',
+    loadComponent: () => import('./pages/privacy-policy/privacy-policy.component').then(m => m.PrivacyPolicyComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
+  },
   { path: '**', redirectTo: '' }
 ];
